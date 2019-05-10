146953fb708143d4dac04e820fa4f9b68a3df6f3
郑锦 <6291@163.com>
2019-05-10 19:12:56 +0800

    修改

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,59 +1,74 @@
-"use script";
+// "use script";
 
-const { LinRouter, NotFound } = require("lin-mizar");
-const { Book } = require("../../models/book");
-const { getSafeParamId } = require("../../libs/util");
-const { Op } = require("sequelize");
-const { BookSearchValidator } = require("../../validators/book");
+// const { LinRouter, NotFound, Forbidden } = require("lin-mizar");
+// const { Book } = require("../../models/book");
+// const { getSafeParamId } = require("../../libs/util");
+// const { Op } = require("sequelize");
+// const { BookSearchValidator, BookCreateOrUpdateValitator } = require("../../validators/book");
+// const { BookDao } = require("../../dao/book");
 
-const bookApi = new LinRouter({
-  prefix: "/v1/book"
-});
+// const bookApi = new LinRouter({
+//   prefix: "/v1/book"
+// });
 
-bookApi.get("/:id", async ctx => {
-  const id = getSafeParamId(ctx);
-  const book = await Book.findOne({
-    where: {
-      id,
-      delete_time: null
-    }
-  });
-  ctx.json(book);
-});
+// const bookDto = new BookDao();
 
-bookApi.get("/search/one", async ctx => {
-  const v = await new BookSearchValidator().validate(ctx);
-  const book = await Book.findOne({
-    where: {
-      title: {
-        [Op.like]: `%${v.get("query.q")}%`
-      },
-      delete_time: null
-    }
-  });
-  if (!book) {
-    throw new NotFound({
-      msg: "没有相关书籍"
-    });
-  };
-  ctx.json(book);
-});
-bookApi.get("/search/all", async ctx => {
-  const v = await new BookSearchValidator().validate(ctx);
-  const book = await Book.findAll({
-    where: {
-      title: {
-        [Op.like]: `%${v.get("query.q")}%`
-      },
-      delete_time: null
-    }
-  });
-  if (book.length === 0) {
-    throw new NotFound({
-      msg: "没有相关书籍"
-    });
-  };
-  ctx.json(book);
-});
+// // id获取书籍
+// bookApi.get("/:id", async ctx => {
+//   const id = getSafeParamId(ctx);
+//   const book = await Book.findOne({
+//     where: {
+//       id,
+//       delete_time: null
+//     }
+//   })
+//   ctx.json(book);
+// });
 
-module.exports = { bookApi };
+// // 关键字搜索单本
+// bookApi.get("/search/one", async ctx => {
+//   const v = await new BookSearchValidator().validate(ctx);
+//   await bookDto.createBook(v);
+//   ctx.success({
+//     msg: "新建图书成功"
+//   });
+// });
+
+// // 关键字搜索所有书籍
+// bookApi.get("/search/all", async ctx => {
+//   const v = await new BookSearchValidator().validate(ctx);
+//   const book = await Book.findAll({
+//     where: {
+//       title: {
+//         [Op.like]: `%${v.get("query.q")}%`
+//       },
+//       delete_time: null
+//     }
+//   });
+//   if (book.length === 0) {
+//     throw new NotFound({
+//       msg: "没有相关书籍"
+//     });
+//   };
+//   ctx.json(book);
+// });
+
+// // 创建书籍
+// bookApi.post("/create", async ctx => {
+//   const v = await new BookCreateOrUpdateValitator().validate(ctx);
+//   console.log(3, v);
+//   const book = await Book.findOne({
+//     where: {
+//       title: v.get("body.title"),
+//       delete_time: null
+//     }
+//   });
+//   if (!book) {
+//     throw new Forbidden({
+//       msg: "图书已存在"
+//     });
+//   }
+//   ctx.json(book);
+// });
+
+// module.exports = { bookApi };
--- app/validators/book.js
+++ app/validators/book.js
@@ -1,14 +1,28 @@
 "use script";
 
-// import { LinValidator } from "lin-mizar";
-
 const { LinValidator, Rule } = require("lin-mizar");
 
 class BookSearchValidator extends LinValidator {
   constructor () {
     super();
     this.q = new Rule("isNotEmpty", "必须传入搜索关键字");
-  };
-};
+  }
+}
 
-module.exports = { BookSearchValidator };
+class BookCreateOrUpdateValitator extends LinValidator {
+  constructor () {
+    super();
+    this.title = new Rule("isNotEmpty", "必须传入书名");
+    this.author = new Rule("isNotEmpty", "必须传入作者");
+    this.summary = new Rule("isNotEmpty", "必须传入图书描述");
+    this.image = new Rule("isLength", "图书插图的url长度必须在0~100之间", {
+      min: 1,
+      max: 100
+    });
+  }
+}
+
+module.exports = {
+  BookSearchValidator,
+  BookCreateOrUpdateValitator
+};
